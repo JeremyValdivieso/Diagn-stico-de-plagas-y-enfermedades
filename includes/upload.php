ce74a6c5b27256a3ee0fdae642af089beaac3b21
JeremyValdivieso <joeljvaldivieso@hotmail.com>
2024-12-15 13:57:56 -0500

    6

--- includes/upload.php
+++ includes/upload.php
@@ -1,13 +1,22 @@
 <?php
-if (isset($_FILES['image'])) {
-    $nombreArchivo = $_FILES['image']['name'];
-    $rutaTemporal = $_FILES['image']['tmp_name'];
-    $rutaDestino = "../uploads/" . $nombreArchivo;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Verificar si se ha subido una imagen
+    if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+        $nombreArchivo = $_FILES['image']['name'];
+        $rutaTemporal = $_FILES['image']['tmp_name'];
+        $rutaDestino = "../uploads/" . $nombreArchivo;
 
-    if (move_uploaded_file($rutaTemporal, $rutaDestino)) {
-        echo "Archivo subido exitosamente.";
+        // Verificar si la imagen se movió correctamente a la carpeta de destino
+        if (move_uploaded_file($rutaTemporal, $rutaDestino)) {
+            echo "Archivo subido exitosamente.";
+        } else {
+            echo "Hubo un error al mover el archivo.";
+        }
     } else {
-        echo "Hubo un error al subir el archivo.";
+        echo "No se seleccionó ninguna imagen o hubo un error en la carga.";
     }
+} else {
+    // Si no es una solicitud POST, redirigir o mostrar un mensaje de error
+    echo "Método de solicitud no permitido.";
 }
 ?>
